0d5d4eec84dca985c27f5193714624412c063974
xiaoyuliu <xla193@sfu.ca>
2016-09-23 21:29:28 -0700

    add data_verify

---
+++ code/lisa-caffe-public/examples/new_lrcn/data_verify.py
@@ -0,0 +1,32 @@
+import numpy as np
+import scipy.io as io
+import cv2 as cv
+import sys, os
+import caffe
+import pdb
+
+model_path = os.path.join( '/cs/vml2/xla193/cluster_video/output/UCF-101/snapshots_singleFrame_RGB/_iter_51201.caffemodel' )
+print "Start loading network.."
+# load net
+net = caffe.Net('/cs/vml2/xla193/cluster_video/code/lisa-caffe-public/examples/new_lrcn/train_test_lstm_RGB.prototxt', 
+				model_path, 
+				caffe.TRAIN)
+
+net.forward()
+
+out = net.blobs['data'].data
+lab = net.blobs['label'].data
+pdb.set_trace()
+
+print "Output shape: {}".format( str(out.shape) )
+print "Output maximum: {}".format( str(out.max()) )
+print "Output minimum: {}".format( str(out.min()) )
+
+
+# result = Image.fromarray( (visual * 255).astype(np.uint8))
+
+io.savemat('out.mat', {"data": visual} )
+
+ 
+
+# result.save('out.png') 
--- code/lisa-caffe-public/examples/new_lrcn/run_lstm_RGB.sh
+++ code/lisa-caffe-public/examples/new_lrcn/run_lstm_RGB.sh
@@ -5,5 +5,6 @@ TOOLS=../../build/tools
 export HDF5_DISABLE_VERSION_CHECK=1
 export PYTHONPATH=.
 
-GLOG_logtostderr=1  $TOOLS/caffe train -solver lstm_solver_RGB.prototxt -weights single_frame_all_layers_hyb_RGB_iter_5000.caffemodel  
+GLOG_logtostderr=1  $TOOLS/caffe train -solver lstm_solver_RGB.prototxt -weights /cs/vml2/xla193/cluster_video/output/UCF-101/snapshots_singleFrame_RGB/_iter_51201.caffemodel
+# -weights single_frame_all_layers_hyb_RGB_iter_5000.caffemodel  
 echo "Done."
