0a6da09d9511daca492b364d9c95bfef59844627
xiaoyuliu <xla193@sfu.ca>
2016-10-05 17:31:32 -0700

    do sum wrt N_triplet instead of mean

--- code/lisa-caffe-public/python/mylayers/triplet_loss_layer.py
+++ code/lisa-caffe-public/python/mylayers/triplet_loss_layer.py
@@ -1,6 +1,8 @@
 import caffe, random, pdb
 import numpy as np 
 import scipy.spatial.distance as ssd
+import time
+import scipy.io as sio
 
 class TripletLossLayer(caffe.Layer):
 
@@ -12,6 +14,7 @@ class TripletLossLayer(caffe.Layer):
         top[0].reshape(1)
 
     def forward(self, bottom, top):
+        # print "forward starts:  ", time.ctime()
         self.feats = bottom[0].data
         # pdb.set_trace()
         labels= bottom[1].data
@@ -81,7 +84,9 @@ class TripletLossLayer(caffe.Layer):
         loss = loss / ( 2 * n_trip )
         top[0].data[...] = loss
 
+        # print("--- %s seconds for forward ---" % (time.time() - start_time))
     def backward(self, top, propagate_down, bottom):
+        # print "backward starts: ", time.ctime()
         feats = self.feats
         anchor = self.anchor
         positive = self.positive
@@ -105,7 +110,14 @@ class TripletLossLayer(caffe.Layer):
                     diffs[aid, i, :] = a*2*(x_n-x_p)
                     diffs[pid, i, :] = a*2*(x_p-x_a)
                     diffs[nid, i, :] = a*2*(x_a-x_n)
+        # start_time = time.time()
+        bottom[0].diff[:][...] = diffs.sum(axis=1)
+    	# print("--- %s seconds for calculating mean ---" % (time.time() - start_time))
+        bottom[0].diff[...] /= bottom[0].num
+        # out = dict()
+        # out['data'] = diffs
+        # sio.savemat('/cs/vml2/xla193/cluster_video/output/UCF-101/snapshots_singleFrame_RGB/3darray.mat', out)
+        # print "saved."
+        # print "update    stops: ", time.ctime()
 
-        for i in range(feats.shape[0]):
-            # pdb.set_trace()
-            bottom[0].diff[i][...] = np.mean(diffs[i,:,:], axis=0, keepdims=True)
+        # print("--- %s seconds for backward ---" % (time.time() - start_time))
